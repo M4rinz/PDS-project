61626010f8a07e06400b51309a44bd3658813fbe
M4rinz <134485532+M4rinz@users.noreply.github.com>
2023-06-14 17:07:27 +0200

    Creato Prova_functionsMatrix.cpp
    Programma simile a Prova_functions.cpp ma con i vectors bidimensionali

---
+++ Prove sparse/Prova_functionsMatrix.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+
+void myStencil(function<int(int)> f, vector<vector<int> >& A, int niter) {
+    for(int k = 0; k < niter; k++) {
+        for(int i = 0; i < A.size(); i++) {  
+            for(int j = 0; j < A[i].size(); j++)
+                A[i][j] = f(A[i][j]);
+        }
+    }
+
+    return;
+}
+
+// Function that prints the matrix
+void print2DVector(const vector< vector<int> > A) {
+    for(int i=0; i<A.size(); i++) {
+        for(int j=0; j<A[i].size(); j++) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    return;
+}
+
+int main(int argc, char* argv[]) {
+    int m = (argc > 1 ? atoi(argv[1]) : 5);
+    int n = (argc > 2 ? atoi(argv[2]) : 10);
+    int niter = (argc > 3 ? atoi(argv[3]) : 3);
+
+    auto f = [] (int x) {return x+1;};
+    function<int(int)> g = [&](int x) {return x+1;};  
+
+
+    vector< vector<int> > A(m, vector<int>(n));
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            A[i][j] = i+j;
+        }            
+    }
+
+    cout << "previous A =" << endl;
+    print2DVector(A);
+
+    cout << "A.size() = " << A.size() << '\t' << "m = " << m << endl;
+    cout << "A[1].size() = " << A[1].size() << '\t' << "n = " << n << endl;
+
+    myStencil(g, A, niter);
+    cout << "A after " << niter << " applications of the function =" << endl;
+    print2DVector(A);
+    
+
+    return 0;
+}
