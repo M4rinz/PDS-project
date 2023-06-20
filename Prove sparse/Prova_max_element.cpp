a8ea7575a1bfded4886e0859f3ff9f1f7edb6737
M4rinz <134485532+M4rinz@users.noreply.github.com>
2023-06-20 11:15:06 +0200

    Creato Prova_max_element.cpp
    Per fare qualche prova su come funziona l'algoritmo std::max_element su vector<pair<int,int>>

---
+++ Prove sparse/Prova_max_element.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    vector<pair<int,int> > v;
+    int max = (argc > 1 ? atoi(argv[1]) : 10);
+
+    for(int i=0; i < max; i++) {
+        v.push_back({(-1)^(i)*i,(int) rand() % 20});
+    }
+
+    cout << "v = [";
+    for(int i = 0; i < max; i++) {
+        cout << " <" << v[i].first << "," << v[i].second << ">";
+    }
+    cout << " ]" << endl;
+
+
+
+
+
+
+    return 0;
+}
