5c799b8ada6cf4a93ff441c64604b450ad43f4ba
M4rinz <134485532+M4rinz@users.noreply.github.com>
2023-06-20 19:44:03 +0200

    Update Prova_max_element
    Aggiunta una parte che, dato un vettore di offset, trova il minimo indice
    di sx, il minimo indice di dx, il minimo indice in alto e il minimo indice in basso.
    Tutto ciò servirà per costruire una "cornice" più piccola possibile
    attorno alla matrice.
    La speranza è che il ciclo for venga vettorizzato...

--- Prove sparse/Prova_max_element.cpp
+++ Prove sparse/Prova_max_element.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <functional>
+#include <cmath>
 
 using namespace std;
 
@@ -9,7 +10,7 @@ int main(int argc, char* argv[]) {
     int max = (argc > 1 ? atoi(argv[1]) : 10);
 
     for(int i=0; i < max; i++) {
-        v.push_back({(-1)^(i)*i,(int) rand() % 20});
+        v.push_back({pow(-1,i)*i,(int) rand() % 20});
     }
 
     cout << "v = [";
@@ -18,14 +19,38 @@ int main(int argc, char* argv[]) {
     }
     cout << " ]" << endl;
 
-    vector<pair<int,int> >::iterator result = max_element(v.begin(), v.end());
+    vector<pair<int,int> >::iterator result1 = max_element(v.begin(), v.end());
 
-    cout << "result = " << (*result).first << endl;
+    cout << "result1 = <" << (*result1).first << "," << (*result1).second << ">" << endl;
+
+    vector<pair<int,int> >::iterator result2 = max_element(v.begin(),
+                            v.end(),
+                            [] (pair<int,int> a, pair<int,int> b) {
+                                return a.second < b.second;
+                                });
+
+    cout << "result2 = <" << (*result2).first << "," << (*result2).second << ">" << endl;
+
+    //vector<pair<int,int> > neigh = {{0,-1},{-1,0},{-2,0},{0,1},{0,2},{1,0},{2,0},{3,0}};
+    vector<pair<int,int> > neigh = {{0,-1},{-1,0},{0,1},{1,0}};
+
+    auto maxLeft = max_element(neigh.begin(),neigh.end(),[] (pair<int,int> a, pair<int,int> b) {return -a.second < -b.second;}); 
+    auto maxTop = max_element(neigh.begin(),neigh.end(),[] (pair<int,int> a, pair<int,int> b) {return -a.first < -b.first;}); 
+
+    cout << "neigh = [";
+    for(int i = 0; i < neigh.size(); i++) cout << " <" << neigh[i].first << "," << neigh[i].second << ">";
+    cout << " ]" << endl;
+    //cout << "left found at index " << distance(neigh.begin(), maxLeft) << " has value <" << (*maxLeft).first << "," << (*maxLeft).second << ">" << endl;
+
+    auto maxRight = max_element(neigh.begin(),neigh.end(),[] (pair<int,int> a, pair<int,int> b) {return a.second < b.second;}); 
+    auto maxBottom = max_element(neigh.begin(),neigh.end(),[] (pair<int,int> a, pair<int,int> b) {return a.first < b.first;}); 
 
 
+    vector<int> margins = {(*maxLeft).second, (*maxTop).first, (*maxRight).second, (*maxBottom).first};
 
-
-
+    cout << "margins = [";
+    for(auto el : margins) cout << el << ", ";
+    cout << "]" << endl;
 
     return 0;
 }
