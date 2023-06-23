f7fdadb274219f7609963aad117984617fae10e0
M4rinz <134485532+M4rinz@users.noreply.github.com>
2023-06-23 18:58:11 +0200

    Update TemplateSeq.cpp
    Ho notato che TemplatePar con un solo thread andava più veloce della versione sequenziale.
    Ho spostato una dichiarazione dall'interno all'esterno del ciclo e,
    come per magia, si sono ridotti di moltissimo i tempi di esecuzione!!
    Cambiamento molto importante questo

--- Sequential/TemplateSeq_stencil.cpp
+++ Sequential/TemplateSeq_stencil.cpp
@@ -14,19 +14,19 @@ std::vector<std::vector<T> >* stencil(
     std::vector< std::vector<T> > B(A.size(), std::vector<T>(A[1].size())); 
 
     for (int k = 0; k < niter; k++) {
-
+        std::vector<T> elems;
         for (int i = 0; i < A.size(); i++) {
             for (int j = 0; j < A[i].size(); j++) {
-                std::vector<T> elems;
+                int ipf, jps;
                 elems.push_back(A[i][j]);
                 for(auto pair : neighborhood) {
-                    int ipf = i+pair.first;
-                    int jps = j+pair.second;
+                    ipf = i+pair.first;
+                    jps = j+pair.second;
                     if((0 <= ipf && ipf < A.size()) && (0 <= jps && jps < A[i].size()))
                         elems.push_back(A[ipf][jps]);  
                 }
                 B[i][j] = f(elems);
-
+                elems = {};
             }            
         }
         std::swap(A,B);
